14d295d47219c53cbd468adda10f53f8cdabb4c1
Carolina Lucca <carolinaplucca@gmail.com>
2020-04-13 16:46:43 -0300

    Criaçao do layout da tela

---
+++ eggplant-brownie/eggplant-brownie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by Carolina Lucca on 13/04/20.
+//  Copyright © 2020 Carolina Lucca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
