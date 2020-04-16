b3ee49aa1c3bb28f5d3edcdcc1bb8e53e231d7a8
Carolina Lucca <carolinaplucca@gmail.com>
2020-04-16 20:38:51 -0300

    Nova Tabela na ViewController com marcadores para selecionar

--- eggplant-brownie/eggplant-brownie/Controllers/ViewController.swift
+++ eggplant-brownie/eggplant-brownie/Controllers/ViewController.swift
@@ -12,13 +12,50 @@ protocol AdicionaRefeicaoDelegate {
     func add(_ refeicao: Refeicao)
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // MARK: - Atributos
     
     var delegate: AdicionaRefeicaoDelegate?
+    var itens: [String] = ["Molho de tomate", "Queijo", "Molho apimentado", "Manjericão"]
+    
+    // MARK: - IBOutlets
     
     @IBOutlet var nomeTextField: UITextField?
     @IBOutlet var felicidadeTextField: UITextField?
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        let linhaDaTabela = indexPath.row
+        let item = itens[linhaDaTabela]
+        
+        celula.textLabel?.text = item
+        
+        return celula
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let celula = tableView.cellForRow(at: indexPath) else { return }
+        
+        if (celula.accessoryType == .none) {
+            celula.accessoryType = .checkmark
+        } else {
+            celula.accessoryType = .none
+        }
 
+    }
+
+    // MARK: - IBActions
+    
     @IBAction func adicionar(_ sender: Any) {
         
 //        if let nomeDaRefeicao = nomeTextField?.text, let felicidadeDaRefeicao = felicidadeTextField?.text {
