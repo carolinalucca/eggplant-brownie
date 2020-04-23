68c34a993d07990bd340e09b57976debdbe0d488
Carolina Lucca <carolinaplucca@gmail.com>
2020-04-23 13:00:18 -0300

    Implementaçao de protocolo NSCoding nas classes Refeiçao e Item

--- eggplant-brownie/eggplant-brownie/Models/Refeicao.swift
+++ eggplant-brownie/eggplant-brownie/Models/Refeicao.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Refeicao: NSObject {
+class Refeicao: NSObject, NSCoding {
     
     // MARK: - Atributos
     
@@ -22,6 +22,20 @@ class Refeicao: NSObject {
         self.nome = nome
         self.felicidade = felicidade
         self.itens = itens
+    }
+    
+    // MARK: NSCoding
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(nome, forKey: "nome")
+        coder.encode(felicidade, forKey: "felicidade")
+        coder.encode(itens, forKey: "itens")
+    }
+    
+    required init?(coder: NSCoder) {
+        nome = coder.decodeObject(forKey: "nome") as! String
+        felicidade = coder.decodeInteger(forKey: "felicidade")
+        itens = coder.decodeObject(forKey: "itens") as! Array<Item>
     }
     
     // MARK: - Funções
